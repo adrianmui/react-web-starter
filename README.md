fb6159f2f7aaa7d35f22c8d32bf5af8d846a59e7
adrian mui <adrian.mui@gmail.com>
2017-06-13 20:46:42 -0500

    init commit, making todo app in react/react-native

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+adrian - 6/13/17
+
 - `npm install`
 - `npm start`
 - create your app in `src/app/Todo.js`
--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    console.log(this.constructor.name);
+
+    this.state = {
+      value: '',
+      items: ['task #1', 'task #2', 'task #3']
+    }
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value.toUpperCase()});
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    let items = [...this.state.items, this.state.value];
+    this.setState({items: items})
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form onSubmit={this.handleClick}>
+          <label>
+          Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
     );
   }
 }
